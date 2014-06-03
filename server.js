2b1bda8bcb852b8b758a6cee0ca95ce5249ac9e7
Fernando Falci <falci@falci.me>
2014-06-02 22:34:51 -0400

    Inicial

---
+++ server.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+var server = http.createServer(app).listen(process.env.PORT || 8888, process.env.IP || "0.0.0.0");
+
+var io = require('socket.io').listen(server);
+
+app.get('/', function (req, res) {
+  res.render(__dirname + '/static/index.jade');
+});
+
+app.use(express.static(__dirname + '/static'));
+
+app.use(function (req, res, next){
+   res.locals.scripts = ['/js/poker.js'];
+   next();
+});
+
+var rooms = [];
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+function findRoom(data){
+    var found = false
+    rooms.forEach(function(room){
+        if(room.name == data.room){
+            found = room;
+        }
+    });
+    
+    return found;
+}
+function removeRoom(room){
+    rooms.forEach(function(item, i){
+        if(item.name == room.name){
+            rooms.remove(i);
+            return true;
+        }
+    });
+    
+}
+
+function createRoom(data){
+    var room = {
+        name: data.room,
+        pass: data.pass,
+        cards: {},
+        sockets: []
+    };
+
+    room.emit = function(name, dados){
+        room.sockets.forEach(function(socket){
+            socket.emit(name, dados);
+        });
+    }
+    room.remove = function(socket){
+        for(var i=0; i<room.sockets.length; i++){
+            if(room.sockets[i].user == socket.user){
+                room.sockets.remove(i);
+                
+                if(!room.sockets.length){
+                    removeRoom(room);
+                }
+                return true;
+            }
+        }
+    }
+    
+    rooms.push(room);
+    
+    return room;
+}
+
+io.sockets.on('connection', function (socket) {
+    
+    socket.on('room', function(data){
+        socket.user = data.user;
+        var room = findRoom(data);
+        
+        if(!room){
+            room = createRoom(data);
+            room.sockets.push(socket);
+            socket.room = room;
+            
+            socket.emit('welcome', {size: room.sockets.length});
+            
+        } else if(room.pass != data.pass){
+            socket.emit('wrong');
+            return false;
+            
+        } else {
+            room.sockets.push(socket);
+            socket.room = room;
+            room.emit('user-in', {size: room.sockets.length, last: socket.user});
+            
+        }
+        
+    }).on('card', function(data){
+        console.log('Carta '+data.value+' de '+socket.user)
+        var room = socket.room;
+        
+        room.cards[ socket.user ] = data.value;
+        
+        var allPlayers = true;
+        room.sockets.forEach(function(socket) {
+            if(!(socket.user in room.cards)){
+                allPlayers = false;
+                return false;
+            }
+        });
+        
+        if(allPlayers){
+            room.emit('finish', room.cards);
+        }
+    }).on('remove-card', function(){
+        delete socket.room.cards[ socket.user ];
+        console.log(['Carta removida', socket.room.cards]);
+        
+    }).on('disconnect', function(){
+        try{
+            var user = socket.user;
+            var room = socket.room;
+            delete room.cards[user];
+            
+            room.remove(socket);
+            room.emit('user-out', {size: room.sockets.length, last: user});
+            
+        }catch(e){
+            console.log('Falha ao remover usuário desconectado');
+            console.error(e);
+        }
+        
+    });
+    
+});
---
+++ static/js/poker.js
@@ -0,0 +1,140 @@
+var socket = io.connect('/');
+const Especial = {
+    Infinito : 101,
+    Question : 102,
+    Time : 103
+};
+  
+socket.on('connect',function(){
+    updateFooter('Conexão OK');
+      
+}).on('wrong', function(){
+    $('#modalLogin .modal-header h4').html("Falha");
+    $('#modalLogin .modal-body p').html("Sala já existe com senha diferente!");
+    $('#modalLogin .modal-footer').removeClass('hidden');
+      
+}).on('welcome', function(data){
+    $('#modalLogin .modal-header h4').html("Bem vindo!");
+    $('#modalLogin .modal-body p').html("Sala criada");
+      
+}).on('user-in', function(data){
+    $('#modalLogin').modal('hide');
+    toast(data.user + ' entrou na sala');
+    updateFooter('Usuários: ' + data.size);
+    $("#login").addClass('hidden');
+    $("#cards").removeClass('hidden');
+      
+}).on('user-out', function(data){
+    toast(data.user + ' saiu da sala');
+    updateFooter('Usuários: ' + data.size);
+    
+    if(data.size == 1){
+        $('#modalLogin').modal('show');
+        $('#modalLogin modal-body p').html('Você está sozinho aqui');
+    }
+      
+}).on('finish', function(data){
+    var min = 100, 
+        max = -1, 
+        soma = 0, 
+        itens = 0, 
+        valores = {};
+    
+    for(var user in data){
+        itens++;
+        var valor = parseFloat(data[user]);
+        console.log(user+" escolheu "+valor);
+        if(valor <= 100){
+            min = Math.min(min, valor);
+            max = Math.max(max, valor);
+            soma += valor;
+        }
+        
+        if(!valores[valor]){
+            valores[valor] = [];
+        }
+        valores[valor].push(user);
+    }
+
+    var html = "";
+    if(valores[Especial.Time]){
+        html = '<span class="btn btn-primary glyphicon glyphicon-time"></span> '+valores[Especial.Time].join(", ")+"<br />";
+        
+    } else if(valores[Especial.Question]){
+        html = '<span class="btn btn-primary">?</span: '+valores[Especial.Question].join(", ")+"<br />";
+        
+    } else if(valores[Especial.Infinito]){
+        html = '<span class="btn btn-primary">&#x221e</span> '+valores[Especial.Infinito].join(", ")+"<br />";
+        
+    } else if(max == min){
+        html = 'Todos iguais: <span class="btn btn-success">'+max+'</span>';
+        
+    } else {
+        console.log(valores);
+        var strMin = (min == 0.5) ? '&#0189' : min;
+        var strMax = (max == 0.5) ? '&#0189' : max;
+        
+        html += '<span class="glyphicon glyphicon-hand-up"></span> <span class="btn btn-primary">'+strMax+"</span> "+valores[max].join(", ")+"<br />";
+        html += '<span class="glyphicon glyphicon-hand-right"></span> <span class="btn btn-primary">'+(soma/itens).toFixed(2)+'</span>';
+        html += '<span class="glyphicon glyphicon-hand-down"></span> <span class="btn btn-primary">'+strMin+"</span> "+valores[min].join(", ")+"<br />";
+    }
+      
+    $('#wait .modal-header h4').html("Resultado");
+    $('#wait .modal-body p').html( html );
+    $('#wait .modal-footer button').addClass('btn-success').html('Ok');
+      
+});
+  
+  
+$(function(){
+    $("#login form").on('submit', function(e){
+        e.preventDefault();
+     
+        socket.emit('room', {
+            room: $("#room").val(), 
+            pass: $("#pass").val(),
+            user:$("#user").val()
+        });
+    
+        if('localStorage' in window){
+            localStorage.setItem('user', $("#user").val());
+            localStorage.setItem('room', $("#room").val());
+        }
+    
+    }).find("[type=submit]").removeAttr('disabled');
+    
+    $("#cards .row button").on('click', function(){
+        var valor = $(this).data('value');
+        socket.emit('card', {
+            value: valor
+        });
+        
+        $('#wait').modal('show');
+        $('#wait .modal-header h4').html("Aguardando outros");
+        
+        if(valor == 0.5){ valor = '&#0189'; }
+        if(valor == Especial.Infinito){ valor = '&#x221e'; }
+        if(valor == Especial.Question){ valor = '?'; }
+        if(valor == Especial.Time){ valor = '<span class="glyphicon glyphicon-time"></span>'; }
+        
+        $('#wait .modal-body p').html( 'Você escolheu: <span class="btn btn-primary">'+valor+'</span>' );
+        $('#wait .modal-footer button').removeClass('btn-success').html('Trocar');
+        
+    });
+    
+    $("#wait").on('hide.bs.modal', function(){
+        socket.emit('remove-card');
+    });
+    
+    if('localStorage' in window){
+        $("#user").val( localStorage.getItem('user') );
+        $("#room").val( localStorage.getItem('room') );
+    }
+  
+});
+
+function toast(){}
+
+function updateFooter(msg){
+    $("#footer p").removeClass("hidden").html(msg);
+}
